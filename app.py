abc36dd04108064df8a731884a37b11e409344e8
Kirill Kuzmenko <igrovaaplaneta@gmail.com>
2023-04-30 05:14:27 +0300

    UPD: Profile - Сделал секцию INF в ЛК - Написал чать роута для обновления данных в БД - Добавил функцию сброса не сохранненных изменений

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from pymongo import MongoClient
 from mongo import *
 import os
 
-
 # Flask config
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = os.getenv('FLASK_KEY')
@@ -41,6 +40,7 @@ def apply_caching(response):
 def index():
     return render_template('main.html')
 
+
 @app.route('/auth')
 def auth():
     if current_user.is_authenticated:
@@ -57,7 +57,66 @@ def profile():
         logout_user()
         return redirect(url_for('auth'))
 
-    return render_template('profile.html', username=current_user.name, usersurname=current_user.surname)
+    DD = current_user.DD
+    MM = current_user.MM
+    YYYY = current_user.YYYY
+
+    if DD == '':
+        DD = "ДД"
+    if MM == '':
+        MM = "ММ"
+    if YYYY == '':
+        YYYY = "ГГГГ"
+    return render_template('profile.html', username=current_user.name, usersurname=current_user.surname,
+                           patronymic=current_user.patronymic, email=current_user.email, DD=DD,
+                           MM=MM, YYYY=YYYY)
+
+
+# Update inf
+@app.route('/changing', methods=['POST', 'GET'])
+def updateInf():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+
+    if request.method == 'POST':
+        try:
+            name = request.form.get('name')
+            surname = request.form.get('surname')
+            patronymic = request.form.get('patronymic')
+            DD = request.form.get('DD')
+            MM = request.form.get('MM')
+            YYYY = request.form.get('YYYY')
+            email = request.form.get('email')
+
+            if 0 < int(MM) <= 12:
+                if int(MM)%2 == 0:
+                    if int(MM) == 2:
+                        if 0 < int(DD) <= 29:
+                            pass
+                        else:
+                            return redirect(url_for('profile'))
+                    else:
+                        if 0 < int(DD) <= 30:
+                            pass
+                        else:
+                            return redirect(url_for('profile'))
+                else:
+                    if 0 < int(DD) <= 31:
+                        pass
+                    else:
+                        return redirect(url_for('profile'))
+            else:
+                return redirect(url_for('profile'))
+
+            return redirect(url_for('profile'))
+
+            # ???????????? КАК ЭТОЙ ХУЙНЕЙ ПОЛЬЗОВАТЬСЯ?!?!
+            # update_record()
+
+        except Exception as e:
+            print(e)
+
+
 
 
 # Login
@@ -84,7 +143,6 @@ def login():
     return render_template('signin.html')
 
 
-
 # Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -99,9 +157,13 @@ def register():
             password = request.form.get('password')
             password2 = request.form.get('password2')
             cur_user = find_user_by_email(email)
+            patronymic = ''
+            DD = ''
+            MM = ''
+            YYYY = ''
 
             if cur_user is None and password == password2 and len(password) >= 1:
-                create_record(name, surname, email, password)
+                create_record(name, surname, patronymic, email, password, DD, MM, YYYY)
                 cur_user = find_user_by_email(email)
                 session['username'] = name
                 session['id'] = cur_user.id
@@ -115,6 +177,7 @@ def register():
             print(e)
 
     return render_template('signin.html')
+
 
 # Logout
 @app.route('/logout', methods=['GET', 'POST'])
