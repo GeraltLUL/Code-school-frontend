088c41fe11bfae10ab3b88c8c33f724d9696eda9
Kirill Kuzmenko <igrovaaplaneta@gmail.com>
2023-05-01 05:06:12 +0300

    UPD: Programs - Начал делать страницу с программами

---
+++ static/js/study.js
@@ -0,0 +1,24 @@
+// let cards = gsap.utils.toArray('.cards')
+//
+//
+//
+// function changeColors(item){
+//     item.style.background = "rgba(0, 80, 255, 0.2)";
+//     item.style.border = "0.2vw #0050FF solid";
+//
+//     document.querySelector(item > div).style.background = "#0050FF";
+// }
+// function resetColors(item){
+//     item.style.background = "none";
+//     item.style.border = "0.2vw white solid";
+// }
+//
+//
+// cards.forEach(item => {
+//     item.addEventListener("mouseenter", () => {
+//         changeColors(item);
+//     });
+//     item.addEventListener("mouseleave", () => {
+//         resetColors(item);
+//     });
+// })
