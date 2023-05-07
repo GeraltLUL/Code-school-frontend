d6b03dcd9f9970b2a1631ed25d4e60512279e727
Kirill Kuzmenko <igrovaaplaneta@gmail.com>
2023-05-07 23:37:32 +0300

    UPD: Study - Практически полностью сделал страницу программ

--- static/js/study.js
+++ static/js/study.js
@@ -1,24 +1,12 @@
-// let cards = gsap.utils.toArray('.cards')
-//
-//
-//
-// function changeColors(item){
-//     item.style.background = "rgba(0, 80, 255, 0.2)";
-//     item.style.border = "0.2vw #0050FF solid";
-//
-//     document.querySelector(item > div).style.background = "#0050FF";
-// }
-// function resetColors(item){
-//     item.style.background = "none";
-//     item.style.border = "0.2vw white solid";
-// }
-//
-//
-// cards.forEach(item => {
-//     item.addEventListener("mouseenter", () => {
-//         changeColors(item);
-//     });
-//     item.addEventListener("mouseleave", () => {
-//         resetColors(item);
-//     });
-// })
+function toEasy(){
+    var hiddenElement = document.querySelector('.easy__lvl')
+    hiddenElement.scrollIntoView({block: "start", behavior: "smooth"});
+}
+function toMedium(){
+    var hiddenElement2 = document.querySelector('.medium__lvl')
+    hiddenElement2.scrollIntoView({block: "start", behavior: "smooth"});
+}
+function toHard(){
+    var hiddenElement3 = document.querySelector('.hard__lvl')
+    hiddenElement3.scrollIntoView({block: "start", behavior: "smooth"});
+}
